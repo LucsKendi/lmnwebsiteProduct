0ed6107303356fe9e801a94c945d989c738d253b
Lucas Kendi <lucaskazuma@hotmail.com>
2024-11-09 21:04:25 -0300

    feat: Página de pesquisas e organização do header

---
+++ projeto/pesquisas/script.js
@@ -0,0 +1,7 @@
+function openFeedbackSurvey() {
+    window.location.href = "https://forms.google.com"; // Substitua com o link correto do Google Forms
+}
+
+function comingSoonAlert() {
+    alert("Pesquisa de satisfação do produto estará disponível em breve!");
+}
